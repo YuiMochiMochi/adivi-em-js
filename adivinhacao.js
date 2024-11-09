41fe8051286c23d52c4afb423be15279fb8a66b9
Emy <141051806+YuiMochiMochi@users.noreply.github.com>
2024-11-08 21:42:52 -0300

    criando a função e as constantes

--- adivinhacao.js
+++ adivinhacao.js
@@ -1,1 +1,7 @@
+function jogar() {
+    console.log("Bem vindo ao ogo de adivinhação!");
 
+    const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    const totalTentativas = 0;
+    const pontos = 1000;
+}
