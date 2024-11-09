bbe14c74fb6ae99eaee7cc1748a6df4f98d750df
Emy <141051806+YuiMochiMochi@users.noreply.github.com>
2024-11-08 22:03:49 -0300

    niveis

--- adivinhacao.js
+++ adivinhacao.js
@@ -2,6 +2,26 @@ function jogar() {
     console.log("Bem vindo ao ogo de adivinhação!");
 
     const numeroSecreto = Math.floor(Math.random() * 100) + 1;
-    const totalTentativas = 0;
-    const pontos = 1000;
-}
+    let totalTentativas = 0;
+    let pontos = 1000;
+
+    console.log("Qual nível de dificuldade?");
+    console.log("(1) Fácil (2) Médio (3) Difícil");
+
+    const nivel = parseInt(prompt("Define o nível: "));
+
+    if (nivel == 1) {
+        totalTentativas = 20;
+    } else if (nivel == 2) {
+        totalTentativas = 10;
+    } else {
+        totalTentativas = 5;
+    }
+
+    
+      
+    
+      console.log("Fim do jogo.");
+    }
+    
+    jogar();
